aeacbde1173fcaee8ee48575815b730cb4b152c9
GameFlame <madest171@gmail.com>
2022-07-25 11:42:35 +0300

    First GLAD expirience: *GLAD added to external *Builded app return OpenGL max and min version in command line and creates yelloq window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
-/*#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
-   string a;
-   getline(cin, a);
-   cout << endl << a;
-   return 0;
-}*/
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -29,11 +21,19 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+   if(!gladLoadGL()){
+      std::cout << "You're noob without GLAD" << std::endl;
+      return -1;
+   }
+   
+   std::cout << "OpenGl" << GLVersion.major << ", " << GLVersion.minor;
+
+   glClearColor(1, 1, 0, 1);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
