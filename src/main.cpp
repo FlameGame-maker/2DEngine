7bea04b22d56b37d2f4876089c6df50608de9f7e
GameFlame <madest171@gmail.com>
2022-07-25 15:39:35 +0300

    By tutorial: -Added sizeCallback(window_ptr, width, height){} -Added keyCallback(window_ptr, key, scancode, action, mode){} By me: -Window with smooth gradient background

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,111 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-int main(void)
-{
-    GLFWwindow* window;
+int windowWidth, windowHeight;
 
-    /* Initialize the library */
-    if (!glfwInit())
+void getResolution() {
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    windowWidth = mode->width;
+    windowHeight = mode->height;
+}
+
+void sizeCallback(GLFWwindow* pWindow, int width, int height) {
+    windowWidth = width;
+    windowHeight = height;
+    glViewport(0, 0,windowWidth, windowHeight);
+}
+
+void keyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(pWindow, 0);
+    }
+}
+
+int main(void) {
+    if (!glfwInit()){//Инициализация glfw
+        std::cout << "glfw initialisation failed" << std::endl;
         return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    getResolution();
+    GLFWwindow* pWindow = glfwCreateWindow(windowWidth, windowHeight, "2DEngine", nullptr, nullptr);//Create a windowed mode window and its OpenGL context
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!pWindow){//Инициализация окна
         glfwTerminate();
+        std::cout << "glfwCreateWIndow failed" << std::endl;
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, sizeCallback);
+    glfwSetKeyCallback(pWindow, keyCallback);
 
-   if(!gladLoadGL()){
-      std::cout << "You're noob without GLAD" << std::endl;
+    glfwMakeContextCurrent(pWindow);//Make the window's context current
+
+   if(!gladLoadGL()){//Инициализация GLAD
+      std::cout << "GLAD initialisation failed" << std::endl;
       return -1;
    }
-   
-   std::cout << "OpenGl" << GLVersion.major << ", " << GLVersion.minor;
+  
+   std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+   std::cout << "OpenGl min version: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+   std::cout << "Current OpenGl version: " << glGetString(GL_VERSION) << std::endl;
 
    glClearColor(1, 1, 0, 1);
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+   float color1 = 1, color2 = 0, color3 = 0;
+   int step = 0;
+   std::chrono::milliseconds timespan(5);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+    while (!glfwWindowShouldClose(pWindow)){//Render loop, отобржается, пока окно не будет закрыто
+        glClearColor(color1, color2, color3, 1);
+        glClear(GL_COLOR_BUFFER_BIT);//Render here
+        glfwSwapBuffers(pWindow);//Swap front and back buffers
+        glfwPollEvents();//Обрабатывает события (позиция курсора, закрытие окна, нажатие клавиш
+        
+        switch (step) {
+            case 0:
+                color2 += 0.005;
+                if (color2 >= 1) step = 1;
+            break;
+            case 1:
+                color1 -= 0.005;
+                if (color1 <= 0) step = 2;
+             break;
+            case 2:
+                color3 += 0.005;
+                if (color3 >= 1) step = 3;
+            break;
+            case 3:
+                color2 -= 0.005;
+                if (color2 <= 0) step = 4;
+            break;
+            case 4:
+                color1 += 0.005;
+                if (color1 >= 1) step = 5;
+            break;
+            case 5:
+                color3 -= 0.005;
+                if (color3 <= 0) step = 0;
+            break;
+        }
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        std::this_thread::sleep_for(timespan);
+        /*std::cout << color1 << " " << color2;
+        std::this_thread::sleep_for(timespan);
+        system("cls");
+        if (color == 1.0) {
+            while (color >= 0.0) {
+                color -= 0.1;
+                std::cout << color;
+                std::this_thread::sleep_for(timespan);
+                std::cout << "\b\b\b";
+            }
+        }*/
     }
 
     glfwTerminate();
